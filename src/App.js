346097fc1d1f1d659d55f225ca05472948424b4d
Daniel Cazal <daniel@otisa.com>
2023-06-27 18:56:54 -0400

    Commit inicial del Front-End para facturación electronica. Cuenta con Login y Validación de credenciales contra las bases Oracle.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+//import ReactDOM from "react-dom";
+
+import "./styles.css";
+
 
 function App() {
+  // React States
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [emps, setEmps] = useState([]);
+
+  const errors = {
+    upass: "invalid username or password",
+    others: "other error"
+  };
+
+  useEffect(() => {
+    fetch('http://localhost:3000/emps_data')
+      .then((r) => r.json())
+      .then((data) => {setEmps(data)})
+      .catch((err) => {
+        console.log(err);
+      });
+  },[]);
+  
+  const handleSubmit = (e) => {
+    //Prevent page reload
+    e.preventDefault();
+
+    var { uname, pass, company} = document.forms[0];
+
+    console.log(uname.value, pass.value, company.value)
+
+    fetch(`http://localhost:3000/empresa/${company.value}/user/${uname.value}/pass/${pass.value}`)
+      .then((data) => {
+        console.log(data)
+          if (data.status === 200) {
+            setIsSubmitted(true);
+          } else {
+            setErrorMessages({name: "upass", message: errors.upass});
+          }
+      })
+      .catch((err) => {
+        setErrorMessages({name: "others", message: errors.others});
+      });
+  };
+
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+  
+
+  // JSX code for login form
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>Username </label>
+          <input type="text" name="uname" required />
+          {renderErrorMessage("uname")}
+        </div>
+        <div className="input-container">
+          <label>Password </label>
+          <input type="password" name="pass" required />
+          {renderErrorMessage("pass")}
+        </div>
+        <div className="input-container">
+          <label>Empresa </label>
+          <select name="company">
+            {emps.map(({codEmpresa, empresa},i) => <option value={codEmpresa} >{empresa}</option>)}
+          </select>
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="login-form">
+        <div className="title">Sign In</div>
+        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+      </div>
     </div>
   );
 }
